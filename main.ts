c46a5d128cfb0cb7041cb1ff19539aa0272fdd28
JolonClarke <jolon.clarke@temple.edu>
2025-09-16 13:47:05 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
-let Counter = 0
 input.onButtonPressed(Button.A, function () {
+    Counter = 0
+    Test_Number += 1
+    Time = 0
+    LL = 0
+    serial.writeValue("Test", Test_Number)
     for (let index = 0; index < 30; index++) {
         basic.pause(2000)
+        serial.writeValue("LL", input.lightLevel())
+        serial.writeValue("Time", Time)
+        LL += input.lightLevel()
+        Time += 2
         if (Counter >= 10) {
             basic.showIcon(IconNames.No)
         } else if (input.lightLevel() < 25) {
@@ -11,6 +19,25 @@ input.onButtonPressed(Button.A, function () {
             Counter = 0
             basic.showIcon(IconNames.Yes)
         }
+        if (Shake) {
+            break;
+        }
     }
     basic.clearScreen()
+    Shake = false
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(LL / Time)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.pause(2000)
+    Shake = true
+    basic.clearScreen()
+    Counter = 0
+})
+let Shake = false
+let LL = 0
+let Time = 0
+let Counter = 0
+let Test_Number = 0
+Test_Number = 0
